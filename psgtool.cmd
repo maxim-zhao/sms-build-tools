4748e2dd0f70c4beba316a8edf4540b46eff57cb
Maxim <maxim@smspower.org>
2019-06-18 09:30:33 +0100

    Remove setup.cmd, wrap PSGTool instead

---
+++ psgtool.cmd
@@ -0,0 +1,2 @@
+rem This wraps PSGTool.jar to make it easier to invoke
+java -jar "%~dp0PSGTool.jar"
--- setup.cmd
+++
@@ -1,6 +0,0 @@
-rem Set the path to include this folder
-path %path%;%~dp0
-rem Set environment variables for all the programs as well
-set bmp2tile=%~dp0\bmp2tile.exe
-set compile=%~dp0\Compile.bat
-set psgtool=%~dp0\psgtool.jar
